8687de6f6ba0a968f20117d916d06d20f8429328
VIVEK KUMAR SHARMA <vivekkumarsharma9746@gmail.com>
2020-12-15 00:20:23 +0530

    Updates

---
+++ precache-manifest.1e5031b9e0c713e86895c8e999c1bbf5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "92c75a12d4f1d8616c0d3716a4e2fd10",
+    "url": "/SpaceX-Recent-Launch-Data/index.html"
+  },
+  {
+    "revision": "00075a53c20c737bd8d1",
+    "url": "/SpaceX-Recent-Launch-Data/static/css/main.e1035070.chunk.css"
+  },
+  {
+    "revision": "f9a8f014551947687a76",
+    "url": "/SpaceX-Recent-Launch-Data/static/js/2.ee91474e.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/SpaceX-Recent-Launch-Data/static/js/2.ee91474e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "00075a53c20c737bd8d1",
+    "url": "/SpaceX-Recent-Launch-Data/static/js/main.6b99f871.chunk.js"
+  },
+  {
+    "revision": "2fd9c0089000e2e6b0d8",
+    "url": "/SpaceX-Recent-Launch-Data/static/js/runtime-main.40076cb0.js"
+  },
+  {
+    "revision": "70cec2311f606d6ca6250a4dc4cde570",
+    "url": "/SpaceX-Recent-Launch-Data/static/media/LandingVideo.70cec231.mp4"
+  }
+]);
