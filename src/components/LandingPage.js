f4e3dd170fffcbe30b0a6a2c77d4eb72e8470ef6
VIVEK KUMAR SHARMA <vivekkumarsharma9746@gmail.com>
2020-12-16 00:20:43 +0530

    fixed nav bar

--- src/components/LandingPage.js
+++ src/components/LandingPage.js
@@ -5,13 +5,16 @@ import SpaceVideo from "../assets/LandingVideo.mp4";
 const Landing = () => {
   return (
     <div className={Styles.landing}>
+      
       <div className={Styles.VideoBox}>
-        <div className={Styles.LandingOverlay}></div>
-        <div>
+      
+      <div className={Styles.LandingOverlay}></div>
           <video autoPlay="autoplay" muted loop="loop">
+          
             <source src={SpaceVideo} type="video/mp4" />
+           
           </video>
-        </div>
+          
       </div>
     </div>
   );
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,28 +1,35 @@
 import React from "react";
-import {Navbar,Nav} from "react-bootstrap";
+import { Navbar, Nav } from "react-bootstrap";
+import Logo from "../assets/spacex-logo.png";
 
 import Styles from "../components/NavigationBar.module.css";
 
-
 const NavigationBar = () => {
+ 
   return (
-    <Navbar bg="transparent" variant="dark" className={Styles.NavigationBar} >
-      <Navbar.Brand href="#home">SpaceX</Navbar.Brand>
-      <Nav className="mr-auto">
-        <a 
-        href="https://spacex.com/vehicles/falcon-9/" target="_blank">Falcon 9</a>
+    <Navbar collapseOnSelect expand="lg" bg="transparent" variant="dark" className={Styles.NavigationBar}>
+     
+      <Navbar.Brand href="#home"><img src={Logo}/></Navbar.Brand>
+      <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+        <Navbar.Collapse id="responsive-navbar-nav">
+      <Nav className="mr-auto" >
+        <Nav.Link href="https://spacex.com/vehicles/falcon-9/" target="_blank">
+          Falcon 9
+        </Nav.Link>
 
-        <a 
-        href="https://www.spacex.com/vehicles/falcon-heavy/" target="_blank">Falcon Heavy</a>
+        <Nav.Link href="https://www.spacex.com/vehicles/falcon-heavy/" target="_blank">
+          Falcon Heavy
+        </Nav.Link>
 
-        <a 
-        href="https://www.spacex.com/vehicles/dragon/" target="_blank">DRAGON</a>
+        <Nav.Link href="https://www.spacex.com/vehicles/dragon/" target="_blank">
+          DRAGON
+        </Nav.Link>
 
         {/* <NavLink className="nav-link" href="#features">Falcon Heavy</NavLink>
         <NavLink className="nav-link" href="#pricing">Dragon</NavLink>
         <NavLink className="nav-link" href="#pricing">Starship</NavLink> */}
-
       </Nav>
+      </Navbar.Collapse>
     </Navbar>
   );
 };
